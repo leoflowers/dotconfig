99dd28416ded2fd2fc96cf1c1527223064d97b6b
Leo Flores <leodflowers@gmail.com>
2025-01-17 12:30:05 -0500

    updated

--- nvim/init.lua
+++ nvim/init.lua
@@ -4,10 +4,7 @@ vim.o.softtabstop = -1
 vim.o.shiftwidth = 0
 vim.o.cursorline = true
 vim.o.number = true
---vim.o.termguicolors = true
 vim.cmd('syntax on')
-
-require'lspconfig'.pyright.setup{}
 
 require("config.lazy")
 
--- nvim/lua/plugins.lua
+++
@@ -1,3 +0,0 @@
-return {
-	'mawkler/modicator.nvim',
-}
